64f5ee38b48857b0b600053925d27356277a5fc5
Mitesh Gandhi <miteshgandhi28@gmail.com>
2024-04-26 14:43:51 +0100

    FInal updates (#15)

--- app/src/main/java/com/cabcta10/weightlossapplication/workerThread/stepCountReward.kt
+++ app/src/main/java/com/cabcta10/weightlossapplication/workerThread/stepCountReward.kt
@@ -1,20 +1,26 @@
 
 import android.content.Context
+import android.icu.util.Calendar
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import com.cabcta10.weightlossapplication.AppDatabase
 import com.cabcta10.weightlossapplication.R
 import com.cabcta10.weightlossapplication.repository.SettingsRepository
+import com.cabcta10.weightlossapplication.repository.StepRepository
 import com.cabcta10.weightlossapplication.repositoryImpl.SettingsRepositoryImpl
+import com.cabcta10.weightlossapplication.repositoryImpl.StepRepositoryImpl
 import com.cabcta10.weightlossapplication.service.NotificationUtil
 import kotlinx.coroutines.flow.firstOrNull
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 
-    private suspend fun fetchStepCountValuesFromDatabase(settingsRepository: SettingsRepository): Int {
-        val s = settingsRepository.getSettings().firstOrNull()
+private suspend fun fetchStepCountValuesFromDatabase(stepRepository: StepRepository): Int {
+        var today = LocalDate.now().format(DateTimeFormatter.ISO_LOCAL_DATE)
+        val s = stepRepository.getbyDate(today.toString()).firstOrNull()
 
             if (s != null) {
-                return 10000 //return s.defaultStepCount .toInt()  // have to change according to the database String Name
+                return s.steps// have to change according to the database String Name
             } else {
                 return 0;
             }
@@ -36,14 +42,15 @@ class StepCountRewardWorker(appContext: Context, workerParams: WorkerParameters
 
 
     override suspend fun doWork(): Result {
-
-
         val settingsRepository = SettingsRepositoryImpl(AppDatabase.getDatabase(applicationContext).settingsDAO())
+        val stepRepository = StepRepositoryImpl(AppDatabase.getDatabase(applicationContext).stepDao())
         val targetStepCount = fetchTargetStepCount(settingsRepository)
-        val stepCount = fetchStepCountValuesFromDatabase(settingsRepository)
+        val stepCount = fetchStepCountValuesFromDatabase(stepRepository)
 
-        if (stepCount == targetStepCount) {
+        if (stepCount >= targetStepCount) {
             NotificationUtil.displayNotification(applicationContext, "Congratulations! You've reached your step goal. Keep up the momentum to stay active.\n", R.drawable.dumbbell)
+        } else {
+            NotificationUtil.displayNotification(applicationContext, "Keep Going! Walking and Running will help you reach your goal for the day.", R.drawable.dumbbell)
         }
         return Result.success()
     }
